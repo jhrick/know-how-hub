fcb0978c80f71579c27347997e1b6ea700e3a673
Jorge Henrique <hj.rickcp@gmail.com>
2024-03-24 18:04:36 -0300

    chore: add table 'sections'

--- backend/src/setup.ts
+++ backend/src/setup.ts
@@ -1,23 +1,30 @@
 import { sql } from "./lib/sql.ts";
 
 async function setup() {
-  await sql`CREATE TABLE IF NOT EXISTS school_presentations (
+  await sql`CREATE TABLE IF NOT EXISTS presentations (
     id      SERIAL PRIMARY KEY,
     title   TEXT UNIQUE,
-    content TEXT
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )`;
+
+  await sql`CREATE TABLE IF NOT EXISTS sections (
+    id SERIAL PRIMARY KEY,
+    content_id INT REFERENCES presentations(id),
+    paragraph_text TEXT,
+    image_url VARCHAR(255)
   )`;
 
   await sql.end();
 }
 
 async function clear() {
-  await sql`TRUNCATE TABLE school_presentations`;
+  await sql`TRUNCATE TABLE presentations, sections`;
 
   await sql.end();
 }
 
 async function deleteTable() {
-  await sql`DROP TABLE school_presentations`;
+  await sql`DROP TABLE presentations, sections`;
 
   await sql.end();
 }
